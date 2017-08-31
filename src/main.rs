4ff7dd49fadc14b9f0f6a37598c48a5fb3f74e6f
palloc <palloc.mknet10@gmail.com>
2017-09-01 00:05:00 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 extern crate iron;
+extern crate rustc_serialize;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
+use rustc_serialize::json;
+use std::io::Read;
+
+#[derive(RustcEncodable, RustcDecodable)]
+struct Greeting {
+    msg: String
+}
 
 fn main() {
+    let mut router = Router::new();
+
+    router.get("/", hello_world, "hello_world");
+    router.post("/set", set_greeting, "set_greeting");
+
     fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello, world")))
+        let greeting = Greeting {
+            msg: "Hello, world".to_string()
+        };
+        let payload = json::encode(&greeting).unwrap();
+        Ok(Response::with((status::Ok, payload)))
     }
 
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
+    fn set_greeting(request: &mut Request) -> IronResult<Response> {
+        let mut payload = String::new();
+        request.body.read_to_string(&mut payload).unwrap();
+        let request: Greeting = json::decode(&payload).unwrap();
+        let greeting = Greeting { msg: request.msg };
+        let payload = json::encode(&greeting).unwrap();
+        Ok(Response::with((status::Ok, payload)))
+    }
+
+    Iron::new(hello_world).http("localhost:3000").unwrap();
+    println!("on 3000");
 }
