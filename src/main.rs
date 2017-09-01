bcbf4471eb4e6ff5b777a6d11a83ba47a3392539
palloc <palloc.mknet10@gmail.com>
2017-09-01 15:47:57 +0900

    add routing

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ use iron::prelude::*;
 mod hello_world;
 mod set_greeting;
 
-
 extern crate postgres;
 use postgres::{Connection, TlsMode};
 
@@ -27,37 +26,11 @@ mod sql;
 use sql::*;
 
 
-
-#[macro_use] extern crate iron;
-#[macro_use] extern crate router;
-extern crate rustc_serialize;
-extern crate urlencoded;
-extern crate iron_sessionstorage;
-
-use iron::status;
-use iron::modifiers::Redirect;
-
-use iron_sessionstorage::traits::*;
-use iron_sessionstorage::SessionStorage;
-use iron_sessionstorage::backends::SignedCookieBackend;
-
-use urlencoded::UrlEncodedBody;
-
-use iron::prelude::*;
-//use router::Router;
-
-mod hello_world;
-mod set_greeting;
-
 struct Login {
     username: String
 }
 
-#[derive(RustcEncodable, RustcDecodable)]
-struct Greeting {
-    msg: String
-}
-
+// ハッシュ管理
 impl iron_sessionstorage::Value for Login {
     fn get_key() -> &'static str { "logged_in_user" }
     fn into_raw(self) -> String { self.username }
@@ -70,26 +43,19 @@ impl iron_sessionstorage::Value for Login {
     }
 }
 
+// ログイン処理
 fn login(req: &mut Request) -> IronResult<Response> {
-    // Already logged in
-    if try!(req.session().get::<Login>()).is_some() {
-        return Ok(Response::with((
-            status::Found,
-            Redirect(url_for!(req, "greet"))
-        )));
-    }
-
     Ok(Response::with((
         status::Ok,
         "text/html".parse::<iron::mime::Mime>().unwrap(),
-        format!("This is an insecure demo, so which username do you want to log in as?<br/> \n\
-                 <form method=post> \n\
-                 <input type=text name=username> \n\
-                 <input type=submit> \n\
-                 </form>")
+
+        // ログインしているかどうか確認する処理を書く
+
     )))
 }
 
+
+// ユーザログイン                       
 fn login_post(req: &mut Request) -> IronResult<Response> {
     let username = {
         let formdata = iexpect!(req.get_ref::<UrlEncodedBody>().ok());
@@ -100,39 +66,45 @@ fn login_post(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Found, Redirect(url_for!(req, "greet")))))
 }
 
+// ログアウト
 fn logout(req: &mut Request) -> IronResult<Response> {
     try!(req.session().clear());
     Ok(Response::with((status::Found, Redirect(url_for!(req, "greet")))))
 }
 
-fn greet(req: &mut Request) -> IronResult<Response> {
-    let login = iexpect!(
-        req.session().get::<Login>().ok().and_then(|x| x),
-        (
-            status::Unauthorized,
-            "text/html".parse::<iron::mime::Mime>().unwrap(),
-            "<a href=/login>Login</a>"
-        )
-    );
-
-    Ok(Response::with((
-        status::Ok,
-        "text/html".parse::<iron::mime::Mime>().unwrap(),
-        format!("hello, {}! <br>\n\
-                 <form method=post action=/logout>\n\
-                 <input type=submit value='log out'>\n\
-                 </form>", login.username)
-    )))
+// ユーザ登録
+fn register(req: &mut Request) -> IronResult<Response> {
 }
 
+// 問題一覧
+fn problems(req: &mut Request) -> IronResult<Response> {
+}
+
+// 問題詳細
+fn problem(req: &mut Request) -> IronReslt<Response> {
+    let ref router = req.extensions.get::<Router>();
+    let ref problem_id = router
+        .unwrap()
+        .find("id")
+        .unwrap();
+
+    return Ok(Response::with(
+        (status::Ok,
+         format!("Hello {}", name).as_str())
+}
+
+
 fn main() {
+    // ルーティング作成
     let router = router!(
-        hello_world: get"/" => hello_world::hello_world,
-        greet: post"/set" => set_greeting::set_greeting,
-        greet: get "/set" => greet,
         login: get "/login" => login,
         login_post: post "/login" => login_post,
         logout: post "/logout" => logout,
+        regist: post "/regist" => regist,
+        user: get "/user" => user,
+        problems: get "/problems" => problems,
+        problem: get "/problem/:id" => problem,
+        
     );
 
     let my_secret = b"verysecret".to_vec();
