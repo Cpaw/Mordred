6b889aa843a4de5829d5efdd12ce22947eb1dc42
palloc <palloc.mknet10@gmail.com>
2017-09-01 23:13:00 +0900

    aiueo

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@
 extern crate rustc_serialize;
 extern crate urlencoded;
 extern crate iron_sessionstorage;
+extern crate mime;
+extern crate csv;
+extern crate serde_json;
+extern crate bodyparser;
+extern crate persistent;
+#[macro_use]
+extern crate serde_derive;
 
+use std::io::Read;
 use iron::status;
 use iron::modifiers::Redirect;
 use router::{Router, url_for};
-
+use rustc_serialize::json;
 use iron_sessionstorage::traits::*;
 use iron_sessionstorage::SessionStorage;
 use iron_sessionstorage::backends::SignedCookieBackend;
-
-use urlencoded::UrlEncodedBody;
 use iron::headers::ContentType;
 use iron::prelude::*;
+use serde_json::Value;
+
+use urlencoded::UrlEncodedBody;
 
 extern crate postgres;
 use postgres::{Connection, TlsMode};
@@ -24,9 +33,23 @@ use sql::*;
 mod hello_world;
 mod set_greeting;
 
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
 
 struct User {
     name: String
+}
+
+
+struct Problem {
+    p1: u16,
+    p2: u16,
+    p3: u16,
+    p4: u16,
+    p5: u16,
 }
 
 // ログイン処理
@@ -89,19 +112,28 @@ fn problem(req: &mut Request) -> IronResult<Response> {
 
 // 問題回答
 fn answer(req: &mut Request) -> IronResult<Response> {
-    let ref router = req.extensions.get::<Router>();
-    let ref problem_id = router
-        .unwrap()
-        .find("id")
-        .unwrap();
+//    let ref router = req.extensions.get::<Router>();
+    let mut payload = String::new();
+    let json_body = req.get::<bodyparser::Json>();
+    println!("{:?}", json_body);
     
+//    let ref problem_id = router
+//        .unwrap()
+//        .find("id")
+//        .unwrap();
+
+    //受け取ったデータをstringでpayloadに格納
+//    req.body.read_to_string(&mut payload);
+//    let v: Value = serde_json::to_value(payload);
+
+//    println!("{}", v);
     Ok(Response::with((
         status::Ok,
         format!("test")
     )))
-}    
+}
               
-fn user(req: &mut Request) -> IronResult<Response>{
+fn user(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((
         status::Ok,
         format!("test")
